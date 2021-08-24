06e388f9a24878577084463938253fc64c51ace8
Michael Zaporozhets <michaelzaporozhets@gmail.com>
2021-08-24 20:57:19 +1000

    setup database connection config and wrap init in db sync

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
+const db = require('./clients/db');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
- 
-app.listen(PORT)
+	res.send('Hello World')
+});
+
+db.sync().then(() => {
+	app.listen(PORT)
+});
